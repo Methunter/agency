31d656d8e8e27a711565ee27d86fde918d17ac46
Methunter <me3sa@me.com>
2015-12-23 13:37:23 +0300

    talkin 'bout mysql

--- sql/actors.sql
+++ sql/actors.sql
@@ -22,11 +22,11 @@ CREATE TABLE IF NOT EXISTS `actors` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `parent_id` int(11) NOT NULL DEFAULT '0',
   `gender` varchar(255) NOT NULL DEFAULT '',
-  `surname` varchar(255) NOT NULL DEFAULT '',
   `name` varchar(255) NOT NULL DEFAULT '',
+  `surname` varchar(255) NOT NULL DEFAULT '',
   `patronymic` varchar(255) NOT NULL DEFAULT '',
   `alias` varchar(255) NOT NULL DEFAULT '',
-  `image` varchar(255) NOT NULL DEFAULT '',
+  `title_image` bulb(255) NOT NULL DEFAULT '',
   `B_date`  datetime NOT NULL DEFAULT '0000-00-00 00:00:00',
   `B_place` varchar(50) NOT NULL DEFAULT '',
   `education` varchar(50) NOT NULL DEFAULT '',
@@ -49,12 +49,15 @@ CREATE TABLE IF NOT EXISTS `actors` (
 -- Dumping data for table `actors`
 --
 
-INSERT INTO `actors` (`id`, `parent_id`, `title`, `name`, `alias`, `image`, `section`, `image_position`, `description`, `published`, `checked_out`, `checked_out_time`, `editor`, `ordering`, `access`, `count`, `params`) VALUES
+INSERT INTO `actors` (`id`, `parent_id`, `gender`, `name`, `surname`, `alias`, `title_image`, `section`, `image_position`, `description`, `published`, `checked_out`, `checked_out_time`, `editor`, `ordering`, `access`, `count`, `params`) VALUES
 (1, 0, '2007 год. Место проведения работ – г. Москва', '', '2007', '', '1', 'left', '', 1, 0, '0000-00-00 00:00:00', NULL, 1, 0, 0, ''),
 (2, 0, '2008', '', '2008', '', '1', 'left', '', 1, 0, '0000-00-00 00:00:00', NULL, 2, 0, 0, ''),
 (3, 0, '2009 год. Место проведения работ – п. Барвиха, Мос', '', '2009-', '', '1', 'left', '', 1, 0, '0000-00-00 00:00:00', NULL, 3, 0, 0, ''),
 (4, 0, '2010 год. Место проведения работ – г. Калининград', '', '2010-------', '', '1', 'left', '', 1, 0, '0000-00-00 00:00:00', NULL, 4, 0, 0, ''),
 (5, 0, '2011', '', '2011', '', '1', 'left', '', 1, 62, '2012-11-08 20:34:25', NULL, 5, 0, 0, ''),
 (6, 0, '2012 - ...', '', '2012-', '', '1', 'left', '', 1, 0, '0000-00-00 00:00:00', NULL, 6, 0, 0, '');
+
+
+
 
 -- --------------------------------------------------------
